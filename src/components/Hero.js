312044bc17cb5d7209cdb1de454664b207da7de7
Daniel Esuola <esuoladaniel002@gmail.com>
2023-01-05 14:14:01 +0100

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+// Icons ====================>>>>>>>>>>>>>>>>
+import { BsArrowRight } from 'react-icons/bs'
+//============================>>>>>>>>>>>>>>>>
 // Images ====================>>>>>>>>>>>>>>>>
 import heroImg from "../assets/img/heroImg.png";
+import BottomBlobs from "../assets/img/bottomBlobs.png";
 //============================>>>>>>>>>>>>>>>>
 
 export default function Hero() {
     return (
-        <div className='h-[100vh] w-screen bg-pink pt-20 relative'>
+        <div className='h-[110vh] max-w-screen bg-pink pt-20 relative'>
             <div className="w-[85vw] mx-auto h-full pt-10 text-center">
                 <div className=' flex gap-5 flex-col mb-10'>
 
@@ -14,9 +18,13 @@ export default function Hero() {
                     <p className='text-2xl w-[40%] text-center mx-auto'>More than just for diapers and wipes, you are in for the big savings too!</p>
 
                 </div>
-                <NavLink to='#' className='border border-red text-white bg-red py-3 px-10 rounded-lg font-bold'>Customize a Bundle now</NavLink>
+                <NavLink to='#' className='border border-red text-white bg-red py-3 rounded-lg font-bold flex items-center gap-2 w-[287px] mx-auto text-center justify-center'>Customize a Bundle now <BsArrowRight fontSize={'1.2em'} /></NavLink>
+
             </div>
-            <img src={heroImg} alt="heroImg" className='absolute bottom-0 w-[70%] right-[15em]' />
+            {/* Hero image */}
+            <img src={heroImg} alt="heroImg" className='absolute bottom-0 w-[80%] right-[11em] z-20' />
+            {/* Backgrounds blobs */}
+            <img src={BottomBlobs} alt="" className='absolute bottom-0 z-10' />
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+// Icons =====================>>>>>>>>>>>>>>>>
+import { FiShoppingCart } from "react-icons/fi";
+import { RxPerson } from "react-icons/rx";
+//=============================>>>>>>>>>>>>>>>
 // Images ====================>>>>>>>>>>>>>>>>
 import Logo from "../assets/img/logo.png";
 //============================>>>>>>>>>>>>>>>>
@@ -14,8 +18,8 @@ export default function Navbar() {
                 </div>
                 {/* Buttons */}
                 <div className="flex gap-5">
-                    <NavLink to='#' className='border border-red text-red py-2 px-10 rounded-lg font-bold'>Cart</NavLink>
-                    <NavLink to='#' className='border border-red text-white bg-red py-2 px-10 rounded-lg font-bold'>Login</NavLink>
+                    <NavLink to='#' className='border border-red text-red py-2 px-10 rounded-lg font-bold flex items-center gap-2'>Cart <FiShoppingCart fontSize={'1.2em'} /></NavLink>
+                    <NavLink to='#' className='border border-red text-white bg-red py-2 px-10 rounded-lg font-bold flex items-center gap-2'>Login <RxPerson fontSize={'1.2em'} /></NavLink>
                 </div>
             </div>
         </div>
